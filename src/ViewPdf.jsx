51791a2cdda2ecbcb46dc8644d9825ef2e4c1f8b
trisssh <samillanotrisha768@gmail.com>
2026-04-07 16:03:18 +0800

    Fixed the Textlayer style issue

--- src/ViewPdf.jsx
+++ src/ViewPdf.jsx
@@ -2,7 +2,9 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { Document, Page, pdfjs } from "react-pdf";
 import { useState } from "react";
 
-// ✅ FIX: worker (important for Vite)
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 export default function ViewPdf() {
